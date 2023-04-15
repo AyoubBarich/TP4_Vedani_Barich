73a5918b2ab31c9191eca2cd26f3f16a33fed822
v20035017 <camille.vedani@outlook.fr>
2023-04-15 02:45:34 +0200

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   Dijkstra.py
    
    Changes not staged for commit:
    	modified:   Graph.py

--- Dijkstra.py
+++ Dijkstra.py
@@ -8,11 +8,25 @@ def Dijkstra(origine:str , graph:Graph):
     path ={}
     queue =[]
     summits[origine] = 0 
-    for summit in graph.getSummits():
+    for summit in graph.getSummits:
         if summit != origine:
             summits[summit] = INF
-    while len(queue) <= graph.size():
-        
+    for summit in graph.getSummits:
+        if  summit not in queue:
+            neighbours = graph.getNeigbours(summit)
+            min = neighbours[0]
+            for neighbour in neighbours:
+                distance=summits[summit]+ graph.getWeight(summit,neighbour)
+                if summits[neighbour]>distance:
+                    summits[neighbour] = distance
+                    if summits[min]>summits[neighbour]:
+                        min = neighbour
+            
+            
+                
+
+
+
     
         
 
